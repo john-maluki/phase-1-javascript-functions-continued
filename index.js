9966c7b5d92265f0dd8a0851e72a6e93a9ec5485
john <johnmaluki58@gmail.com>
2023-06-13 22:19:51 +0300

    Completed assignment on continued functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(str = "roller-skate") {
+  return `This Saturday, I want to ${str}!`;
+}
+
+function mondayWork(str = "go to the office") {
+  return `This Monday, I will ${str}.`;
+}
+
+function wrapAdjective(string = "*") {
+  return function (str = "special") {
+    return `You are ${string}${str}${string}!`;
+  };
+}
